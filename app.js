d471ec626219314fb3815de1e069f93af3ae3dcf
mzkki <08harismu@gmail.com>
2022-03-28 20:44:03 +0800

    add api aldhan and get date

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+function prayerTimes(latitude, logitude){
+    fetch('https://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+logitude+'&method=2')
+    .then(response => response.json())
+    .then(function(response){
+        let date = new Date();
+        let today = date.getDate() - 1;
+        console.log(response.data[today]);
+    });
+}
+
+function success(position){
+    prayerTimes(position.coords.latitude, position.coords.longitude);
+}
+
+function error(){
+    alert('Unable to retrieve your location');
+}
+
+function userLocation(){
+    if(!navigator.geolocation){
+        alert('Geolocation is not supported by your browser');
+    }else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index(){
     let app = document.getElementById('app');
     let h3  = document.createElement('h3');
     h3.innerHTML = 'Prayer Times';
 
     app.appendChild(h3); 
+
+    userLocation()
 }
 index()
